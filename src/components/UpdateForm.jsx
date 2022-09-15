71a61d2d8d40fc42147a224d3f0a1e11aa1128d6
abhinavck0001 <105871927+abhinavck0001@users.noreply.github.com>
2022-09-15 18:09:44 +0530

    Add files via upload

---
+++ src/components/UpdateForm.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react'
+
+function UpdateForm({updateData,changeTask,updateTask,cancelUpdate}){
+
+  return(
+    <div>
+    <div className='row'>
+    <div className='col'>
+      <input className='form-control form-control-lg'
+      value={updateData && updateData.title}
+      onChange={(e)=> changeTask(e)}
+      />
+    </div>
+    <div className='col-auto'>
+      <button
+      onClick={updateTask}
+        className='btn btn-lg btn-success mr-20'>Update
+      </button>
+      <button 
+      onClick={cancelUpdate}
+      className='btn btn-lg btn-warning'>Cancel</button>
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default UpdateForm
